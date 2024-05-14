a37fd46e82b39e8fd7cba64f28809ae57578b160
Hoang NK <n.khanhhoang@gmail.com>
2024-05-14 08:31:50 +0700

    add shadcn-ui

--- app/api/[[...route]]/route.tsx
+++ app/api/[[...route]]/route.tsx
@@ -1,12 +1,55 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import { env } from 'hono/adapter'
+import { Redis } from '@upstash/redis'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
+type EnvConfig = {
+  UPSTASH_REDIS_REST_TOKEN: string
+  UPSTASH_REDIS_REST_URL: string
+}
+app.get('/search', async (context) => {
+  try {
+    const { UPSTASH_REDIS_REST_TOKEN, UPSTASH_REDIS_REST_URL } =
+      env<EnvConfig>(context)
 
-app.get('/search', (response) => {
-  return response.json({})
+    const redis = new Redis({
+      token: UPSTASH_REDIS_REST_TOKEN,
+      url: UPSTASH_REDIS_REST_URL
+    })
+
+    const query = context.req.query('q')?.toUpperCase()
+
+    if (!query) {
+      return context.json({ message: 'No query provided', status: 400 })
+    }
+
+    const res: string[] = []
+    const rank = await redis.zrank('terms', query)
+
+    if (rank !== null && rank !== undefined) {
+      const range = await redis.zrange<string[]>('terms', rank, rank + 100)
+
+      for (const term of range) {
+        if (!term.startsWith(query)) {
+          break
+        }
+
+        if (term.endsWith('*')) {
+          res.push(term.slice(0, -1))
+        }
+      }
+    }
+
+    return context.json({
+      results: res
+    })
+  } catch (error) {
+    console.log('Error:', error)
+    return context.json({ message: 'Something went wrong', status: 500 })
+  }
 })
 
 export const GET = handle(app)
--- app/page.tsx
+++ app/page.tsx
@@ -21,20 +21,26 @@ export default function Home() {
 
   useEffect(() => {
     if (inputRef.current) {
-      console.log('focus')
       inputRef.current.focus()
     }
   }, [])
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <input
-        ref={inputRef}
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        className="px-3 py-2 border border-gray-300 w-1/2 rounded-lg"
-      />
-    </div>
+    <main className="h-screen w-screen grainy">
+      <div className="flex flex-col gap-6 items-center pt-32 duration-500 animate-in animate fade-in-5 slide-in-from-bottom-2.5">
+        <h1 className="text-5xl tracking-tight font-bold">Speed Search</h1>
+        <p className="text-zinc-600 text-lg max-w-prose text-center">
+          A high-performance API built with Hono, Next.js and Cloudflare.
+          <br /> Type a query blow and get your results in milliseconds.
+        </p>
+        <input
+          ref={inputRef}
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="px-3 py-2 border border-gray-300 w-1/2 rounded-lg"
+        />
+      </div>
+    </main>
   )
 }
